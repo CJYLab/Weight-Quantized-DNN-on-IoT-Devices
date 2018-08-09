a0cd1fce3118249ec443a575ece182b3f9e0ae25
Jingyong Cai <1769395594@qq.com>
2018-08-10 03:03:06 +0900

    Update weightQuantization.py

--- weightsQuantization_Distribution/weightQuantization.py
+++ weightsQuantization_Distribution/weightQuantization.py
@@ -32,7 +32,7 @@ Round(x)
 		ROUND DOWN
 
 """
-
+from __future__ import division
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -40,9 +40,7 @@ import math
 import collections
 from collections import Counter
 import seaborn as sns
-import sys
-sys.path.append('../projectionProfileAlgorithm')
-import projectionProfileMultiProcessVersion as pro
+from resize_training_input_neurons import projectionProfileMultiProcessVersion as pro
 
 pro.dataPreparation()
 pro.trainMnist()
@@ -65,7 +63,7 @@ def Clip(x, maxima, minima):
 	if(x <= MIN):
 		return MIN
 	elif(x >= FSR):
-		return FSR-1
+		return FSR - 1
 	else:
 		return x
 
@@ -82,15 +80,69 @@ vRound = np.vectorize(Round)
 vClip = np.vectorize(Clip)
 
 
+
+#mask (sign bits of the original weights, these informatin will get lost after quantization)
+def signBitsMaskArray(layer):
+	mask  = layer < 0
+	mask = mask * (-1)
+	mask[mask > -1] = 1
+	return mask
+
 #for layer1:
 
 
 quantized_weights_layer1 = vClip(vRound(np.log2(abs(weights_layer1))), maxima1, minima1)
-print(quantized_weights_layer1)
+layer1_mask = signBitsMaskArray(weights_layer1)
+anti_quantized_weights_layer1 = np.power(2.0,quantized_weights_layer1)
+anti_quantized_weights_layer1 = anti_quantized_weights_layer1 * layer1_mask
 
 
 
 #for layer2:
 
 quantized_weights_layer2 = vClip(vRound(np.log2(abs(weights_layer2))), maxima2, minima2)
-print(quantized_weights_layer2)
+layer2_mask = signBitsMaskArray(weights_layer2)
+anti_quantized_weights_layer2 = np.power(2.0, quantized_weights_layer2)
+anti_quantized_weights_layer2 = anti_quantized_weights_layer2 * layer2_mask
+
+
+# print("layer1 mask")
+# print(layer1_mask)
+#
+# print("weights_layer1")
+# print(weights_layer1)
+#
+# print("anti_quantized_weights_layer1")
+# print(anti_quantized_weights_layer1)
+
+
+randint = np.random.randint(0,10000,size=1000)
+
+resizedInputs_t = np.array(pro.resizedInputs_t, dtype=np.float32)
+labels_t = np.array(pro.labels_t, dtype=np.int32)
+test_items = resizedInputs_t[randint]
+test_items_labels = labels_t[randint]
+
+
+#accuracy tests
+
+
+#forward computation
+count = 0
+for index, inputs in enumerate(test_items):
+	layer1_out = np.dot(anti_quantized_weights_layer1, inputs) + biases_layer1
+	#relu here
+	layer1_out[layer1_out < 0] = 0
+	layer2_out = np.dot(anti_quantized_weights_layer2,layer1_out) + biases_layer2
+	output = np.argmax(layer2_out)
+	print(int(output))
+	print(int(test_items_labels[index]))
+	print("----------")
+	if int(output) == int(test_items_labels[index]):
+		count = count+1
+
+accuracy = count/1000
+print("accuracy")
+print(accuracy)
+
+#visualization
